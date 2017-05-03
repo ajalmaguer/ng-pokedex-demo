e51df6877ced425f3718f1e49a112688a66e489c
AJ Almaguer <ajalmaguer@gmail.com>
2017-05-03 01:18:56 -0700

    fixed linting errors

--- src/app/poke.service.ts
+++ src/app/poke.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http'
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PokeService {
 	// make api calls in the service
 
-	test = 'hello from the poke service'
-	selectedPokemon = {}
+	test = 'hello from the poke service';
+	selectedPokemon: any = {};
 
 	constructor(private http: Http) { }
 
 	getAllPokemon() {
 		return this.http.get('http://pokeapi.co/api/v2/pokemon/')
 						.map(res => {
-							return res.json().results
-						})
+							return res.json().results;
+						});
 	}
 
 	getPokeDetails(pokemon) {
 		return this.http.get(pokemon.url)
 			.map(res => {
-				return res.json()
+				return res.json();
 				// return res.json().results
 			})
 	}
@@ -29,6 +29,6 @@ export class PokeService {
 }
 
 interface Pokemon {
-	name: string
-	url: string
+	name: string;
+	url: string;
 }
