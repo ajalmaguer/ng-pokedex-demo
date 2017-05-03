4bfb3cb2026ee4d12eb9f8a181d4706209d69c47
AJ Almaguer <aj.almaguer@usaepay.com>
2017-05-03 12:46:07 -0700

    created an empty ui-library module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,35 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// modules
+import { UiLibraryModule } from './ui-library/ui-library.module';
+
+// services
 import { PokeService } from './poke.service'
 
+// components
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
 import { CollectionComponent } from './collection/collection.component';
 
 @NgModule({
-  declarations: [ // componentes go here
-    AppComponent,
-    ListComponent,
-    DetailsComponent,
-    CollectionComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-  ],
-  providers: [ // services go here
+	declarations: [ // componentes go here
+		AppComponent,
+		ListComponent,
+		DetailsComponent,
+		CollectionComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		UiLibraryModule,
+	],
+	providers: [ // services go here
 	PokeService
 	// spriteSerivce
-  ],
-  bootstrap: [AppComponent]
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
