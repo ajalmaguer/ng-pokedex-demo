599af353c7c8eaa3419dd18f6a83f562044a8a19
AJ Almaguer <aj.almaguer@usaepay.com>
2017-05-03 13:17:41 -0700

    created thumbnail and button component

--- src/app/ui-library/ui-library.module.ts
+++ src/app/ui-library/ui-library.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './button/button.component';
+import { ThumbnailComponent } from './thumbnail/thumbnail.component';
 
 @NgModule({
 	imports: [
 		CommonModule
 	],
-	declarations: []
+	declarations: [
+		ButtonComponent, 
+		ThumbnailComponent
+	],
+	exports: [
+		ButtonComponent, 
+		ThumbnailComponent
+	]
 })
 export class UiLibraryModule { }
